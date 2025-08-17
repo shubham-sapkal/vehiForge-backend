b605a73039d1b75805ba291cb97f9b271bc2865e
shubham-sapkal <shubhamsapkal7052@gmail.com>
2025-08-17 19:29:03 +0530

    [INIT] Init Commit - Shubham

---
+++ BookingService/src/main/kotlin/com/vehiforge/bookingService/BookingServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.vehiforge.userService
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookingServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<BookingServiceApplication>(*args)
+}
---
+++ UserService/src/main/kotlin/com/vehiforge/userService/core/health/controllers/HealthController.kt
@@ -0,0 +1,20 @@
+package com.vehiforge.userService.core.health.controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/health")
+class HealthController {
+
+    @GetMapping
+    fun default(): String {
+        return "User Service is up and running"
+    }
+
+    // Liveness API
+
+    // Readiness API
+
+}
